390e3f56c43e797328dc94729f010d3d168b380a
Richard Astbury <richard.astbury@gmail.com>
2017-02-21 14:47:22 +0000

    command line switches

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 /*
 ## todo:
 
-* report on package versions
 * allow the user to customise the starting directory
-* add a switch for only seeing the orphans
-* add a switch to turn on package names starting with System.
 * add a switch for a package id filter
+* include PackageReference format https://github.com/NuGet/Home/wiki/PackageReference-Specification
 
 */
 
@@ -13,8 +11,17 @@ var packagesConfig = require('./packages.config.js');
 var nuspec = require('./package.nupkg.js');
 var packages = require('./packages.js');
 var archy = require('archy');
+var colours = require('colors');
 
 var dir = process.cwd();
+
+function hasFlag(name){
+    return !!process.argv.filter(x => x === '--' + name).length;
+}
+
+var hideVersion = hasFlag('hideVersion');
+var showSystem = hasFlag('showSystem');
+var onlyTopLevel = hasFlag('onlyTopLevel');
 
 var packageFolder = packages.findPackageFolder(dir);
 if (!packageFolder){
@@ -23,11 +30,16 @@ if (!packageFolder){
     return;
 }
 
-var packages = packagesConfig.list(dir).filter(x => x.id.indexOf('System.') !== 0);
+var packages = packagesConfig.list(dir);
+
+if (!showSystem){
+    packages = packages.filter(x => x.id.indexOf('System.') !== 0)
+}
+
 var packageDictionary = {};
 packages.forEach(x => {
     packageDictionary[x.id] = x;
-    x.label = x.id;
+    x.label = x.id + " " + (hideVersion ? "" : x.version.green);
 });
 
 packages.forEach(x => {
@@ -46,9 +58,15 @@ packages.forEach(x => {
     });
 });
 
-var head = {
-    label : "packages.config",
-    nodes : packages.filter(x => !x.used)
-};
 
-console.log(archy(head));
+if (onlyTopLevel){
+    packages.filter(x => !x.used).forEach(x => {
+        console.log(x.label);
+    });
+} else {
+    var head = {
+        label : "packages.config",
+        nodes : packages.filter(x => !x.used)
+    };
+    console.log(archy(head));
+}
