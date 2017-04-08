f0416bb776d2e263b50f23b0794442971bc93abe
michael@fringefy.com <michael@fringefy.com>
2017-04-08 14:29:54 +0300

    print err when bad format

--- packages.config.js
+++ packages.config.js
@@ -3,24 +3,30 @@ var path = require('path');
 var parseXml = require('xml2js').parseString;
 var safeBufferRead = require('./safeBufferRead');
 
-module.exports.list = function(dir){
+module.exports.list = function (dir) {
 
-    if (!fs.existsSync(path.join(dir,'packages.config'))){
+    if (!fs.existsSync(path.join(dir, 'packages.config'))) {
         return null;
     }
 
-    var xml = safeBufferRead(fs.readFileSync(path.join(dir,'packages.config')));
+    var xml = safeBufferRead(fs.readFileSync(path.join(dir, 'packages.config')));
 
     var parsedOutput;
     parseXml(xml, function (err, result) {
         parsedOutput = result;
     });
 
-    return parsedOutput.packages.package.map(x => {
-        return {
-            id : x.$.id,
-            version: x.$.version,
-            targetFramework : x.$.targetFramework
-        }
-    });
+    try {
+        return parsedOutput.packages.package.map(x => {
+            return {
+                id: x.$.id,
+                version: x.$.version,
+                targetFramework: x.$.targetFramework
+            }
+        });
+    }
+    catch(err){
+        console.log("Cannot parse 'packages.config'. Is it in a valid format?");
+        return false;
+    }
 }
